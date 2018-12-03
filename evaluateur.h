0607866c58385b6110fa91ce4e443dcedda48f90
Axel I <34034880+Irthene@users.noreply.github.com>
2018-12-03 10:50:03 +0100

    Update evaluateur.h

--- evaluateur.h
+++ evaluateur.h
@@ -2,5 +2,12 @@
 #define EVALUATEUR_H_INCLUDED
 
 
+typedef struct Node
+{
+    typejeton jeton;
+    struct Node *pjeton_preced;
+    struct Node *pjeton_suiv;
+} Node;
+typedef Node *Arbre;
 
 #endif // EVALUATEUR_H_INCLUDED
